5131a57b8af4541c1d632c397dcafce09dfadaf2
Manuel José Sánchez Allan <contacto@manuelsanchez.com.mx>
2017-02-12 17:56:21 -0700

    devise routes working without root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      passwords: 'users/passwords',
-      registrations: 'users/registrations'
-  }
+  #devise_for :users, controllers: {
+  #	sessions: "users/sessions#new",
+  #    sessions: 'users/sessions',
+  #    passwords: 'users/passwords',
+  #    registrations: 'users/registrations'
+  #}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  #root to: "home#index"
 
-get '/resource/sign_in', to: 'users/sessions#new'
+#devise_scope :user do
+#  root to: "devise/sessions#new"
+#end
+#devise_scope :user do
+#get '/resource/sign_in', to: 'users/sessions#new'
+#end
+devise_for :users
+#root to:  '/users/sign_in', to: 'devise/sessions#new'
 end
